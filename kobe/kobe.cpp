303f656294c703449a2c8e980ddda89b33976ac4
pierreantoine <pierreantoine.guillaume+github@gmail.com>
2018-08-28 15:04:33 +0200

    refactor and static type verification

--- kobe/kobe.cpp
+++ kobe/kobe.cpp
@@ -8,60 +8,84 @@
 using namespace std;
 
 vector<string> arguments;
-int success_rate;
-size_t attempt_count_per_session;
-size_t session_count = 100000;
-
-size_t success_count=0;
-
-string usage(){
+int            success_rate;
+size_t         attempt_count_per_session;
+size_t         session_count = 100000;
+size_t success_count = 0;
+string usage()
+{
     return "./a.out success_rate attempt_count_per_session [ session_count = 100000  ]";
 }
 
-bool makeAttempt(){
-    return ((rand()+1)%100) < success_rate;
+bool makeAttempt()
+{
+    return ((rand() + 1) % 100) < success_rate;
 }
 
-bool runSession(){
-    for (size_t i=0;i<attempt_count_per_session;++i)
+bool runSession()
+{
+    for (size_t i = 0; i < attempt_count_per_session; ++i)
+    {
         if (makeAttempt())
+        {
             return true;
+        }
+    }
     return false;
 }
 
-void assignValues(int argc, char ** argv){
-    arguments = vector<string>(argv+1,argv+argc);
-    if (arguments.size()<2){
+void assignValues(int argc, char **argv)
+{
+    arguments = vector<string>(argv + 1, argv + argc);
+    if (arguments.size() < 2)
+    {
         throw std::invalid_argument(usage());
     }
 
-    srand(time(nullptr));
-    success_rate = stoi(arguments[0]);
-    attempt_count_per_session = stoi(arguments[1]);
-    
-    if (arguments.size()==3){
-        session_count = stoi(arguments[2]);
+    srand(static_cast<unsigned int>(time(nullptr)));
+    success_rate              = stoi(arguments[0]);
+    attempt_count_per_session = static_cast<std::size_t >(stoi(arguments[1]));
+
+    if (arguments.size() == 3)
+    {
+        session_count = static_cast<std::size_t >(stoi(arguments[2]));
     }
 }
 
-void startTrials(){
-    for (size_t i = 0; i< session_count; ++i){
-        if (runSession()){
-            success_count+=1;
+void startTrials()
+{
+    for (size_t i = 0; i < session_count; ++i)
+    {
+        if (runSession())
+        {
+            success_count += 1;
         }
     }
 }
 
-void displayResults(){
-    cout << "After " << session_count << " there has been " << success_count << " successes. Which means a rate of " << float(success_count)/session_count << "%" << endl;
+void displayResults()
+{
+    cout
+            << "After "
+            << session_count
+            << " there has been "
+            << success_count
+            << " successes. Which means a rate of "
+            << float(success_count) / session_count
+            << "%"
+            << endl;
 }
 
-int main(int argc, char** argv){
-    try{
-    assignValues(argc, argv);
-    startTrials();
-    displayResults();
-    } catch(std::exception & e){
+int main(int argc, char **argv)
+{
+    try
+    {
+        assignValues(argc, argv);
+        startTrials();
+        displayResults();
+    }
+    catch (std::exception &e)
+    {
         std::cerr << e.what() << endl;
         return 1;
     }
